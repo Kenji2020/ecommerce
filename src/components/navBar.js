bd6dfe30cb6a4303034e54ef23ffad7bdabd9fdc
Victor2020 <83725041+Kenji2020@users.noreply.github.com>
2022-07-10 18:51:47 -0400

    request 2

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Nav,Navbar,Container, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 export const NavBar = () =>{
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar className="navbar" expand="lg">
             <Container>
                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav>
+                        <Nav.Link href="#home" className="navbar-nav">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>

7bb9f6856294c9960f54f1594ee55e0ac27d0635
dlwlrmla <joakomask@gmail.com>
2022-07-10 19:59:11 -0400

    a

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,7 +4,7 @@ export const NavBar = () =>{
     return (
         <Navbar className="navbar" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">React alo -Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav>
